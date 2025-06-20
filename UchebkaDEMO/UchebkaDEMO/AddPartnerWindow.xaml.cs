7741c8cdd7f2d24b2a0c87f07dafb06c3f99b13a
Rolan <109095650+Dac0ma@users.noreply.github.com>
2025-06-20 06:31:57 +0500

    help

---
+++ UchebkaDEMO/UchebkaDEMO/AddPartnerWindow.xaml.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UchebkaDEMO
+{
+    /// <summary>
+    /// Логика взаимодействия для AddPartnerWindow.xaml
+    /// </summary>
+    public partial class AddPartnerWindow : Window
+    {
+        string connectionString = @"Data Source=DESKTOP-DC3OGQJ; Initial Catalog=DemoUCheb;Integrated Security=True"; // Замените на вашу строку подключения к БД
+
+        public AddPartnerWindow()
+        {
+            InitializeComponent();
+            PhoneTextBox.Text = "+7 ";
+            LoadPartners();
+            // Получение текущей даты и времени
+            DateTime currentDateTime = DateTime.Now;
+            // Форматирование даты
+            string formattedDate = currentDateTime.ToString("dd-MM-yyyy");
+            DateToday_Label.Content = $"Сегодня: {formattedDate}";
+        }
+
+        private void Back_Button_Click(object sender, RoutedEventArgs e)
+        {
+            ViewPartnersWindow viewPartnersWindow = new ViewPartnersWindow();
+            viewPartnersWindow.Show();  
+            this.Close();
+        }
+
+        private bool ValidInfo()
+        {
+            string partnerName = PartnerNameTextBox.Text;
+            if (CompanyTypeComboBox.SelectedItem == null)
+            {
+                MessageBox.Show("Пожалуйста, выберите тип партнера.");
+                return false;
+            }
+            int partnerTypeId = ((KeyValuePair<int, string>)CompanyTypeComboBox.SelectedItem).Key;
+            string directorName = DirectorTextBox.Text;
+            string rating = RatingTextBox.Text;
+            string phoneNumber = PhoneTextBox.Text;
+            string email = EmailTextBox.Text;
+            string address = AddressTextBox.Text;
+
+            // Валидация номера телефона
+            if (phoneNumber.StartsWith("+7"))
+            {
+                phoneNumber = phoneNumber.Substring(3); // Убираем "+7 "
+            }
+            if (phoneNumber.Length != 13)
+            {
+                MessageBox.Show("Номер телефона должен быть в формате 493 123 45 67");
+                return false;
+            }
+
+            // Валидация других полей
+            if (string.IsNullOrWhiteSpace(partnerName) || string.IsNullOrWhiteSpace(directorName) || string.IsNullOrWhiteSpace(address))
+            {
+                MessageBox.Show("Пожалуйста, заполните все обязательные поля.");
+                return false;
+            }
+
+            return true;
+        }
+
+        private void LoadPartners()
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    sqlConnection.Open();
+                    string query = "SELECT id, name FROM Company_types";
+                    SqlCommand command = new SqlCommand(query, sqlConnection);
+                    SqlDataReader reader = command.ExecuteReader();
+                    CompanyTypeComboBox.Items.Clear();
+
+                    while (reader.Read())
+                    {
+                        CompanyTypeComboBox.Items.Add(new KeyValuePair<int, string>(
+                            (int)reader["id"],
+                            reader["name"].ToString()
+                        ));
+                    }
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка загрузки типов партнёра: {ex.Message}");
+                }
+            }
+        }
+
+        private void SaveInfo_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (ValidInfo() == true)
+            {
+                string partnerName = PartnerNameTextBox.Text;
+                int partnerTypeId = ((KeyValuePair<int, string>)CompanyTypeComboBox.SelectedItem).Key;
+                string directorFullName = DirectorTextBox.Text;
+                string rating = RatingTextBox.Text;
+                string phoneNumber = PhoneTextBox.Text;
+                string email = EmailTextBox.Text;
+                string address = AddressTextBox.Text;
+
+                string[] nameParts = directorFullName.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                // Проверяем количество частей
+                if (nameParts.Length < 2 || nameParts.Length > 3)
+                {
+                    MessageBox.Show("Пожалуйста, введите Фамилию, Имя и Отчество через пробел.");
+                    return; // Выход из метода, если формат неправильный
+                }
+
+                string firstName = nameParts[1]; // Имя
+                string lastName = nameParts[0]; // Фамилия
+                string patronymic = nameParts.Length == 3 ? nameParts[2] : ""; // Отчество (если есть)
+
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    try
+                    {
+                        conn.Open();
+                        string query = "INSERT INTO Partners (name, legal_address, firstname, " +
+                            "secondname, patronymic, phone, email, rating, type_id) " +
+                            "VALUES (@partnerName, @address, @firstname, @lastname, @patronymic, @phone, @email, @rating, @typeId)";
+                        SqlCommand cmd = new SqlCommand(query, conn);
+                        cmd.Parameters.AddWithValue("@partnerName", partnerName);
+                        cmd.Parameters.AddWithValue("@address", address);
+                        cmd.Parameters.AddWithValue("@firstname", firstName);
+                        cmd.Parameters.AddWithValue("@lastname", lastName);
+                        cmd.Parameters.AddWithValue("@patronymic", patronymic);
+                        cmd.Parameters.AddWithValue("@phone", phoneNumber.Substring(3));
+                        cmd.Parameters.AddWithValue("@email", email);
+                        cmd.Parameters.AddWithValue("@rating", rating);
+                        cmd.Parameters.AddWithValue("@typeId", partnerTypeId);
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                        {
+                            MessageBox.Show("Партнер успешно добавлен");
+                            // Очистить поля после успешного добавления
+                            PartnerNameTextBox.Clear();
+                            CompanyTypeComboBox.SelectedIndex = -1;
+                            DirectorTextBox.Clear();
+                            RatingTextBox.Clear();
+                            PhoneTextBox.Clear();
+                            AddressTextBox.Clear();
+                            EmailTextBox.Clear();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Не удалось добавить партнера.");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Ошибка добавления данных: {ex.Message}");
+                    }
+                }
+            }
+        }
+
+        private void PhoneTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsTextAllowed(e.Text);
+        }
+
+        private void PhoneTextBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (PhoneTextBox.SelectionStart < 4 && (e.Key == Key.Back || e.Key == Key.Delete))
+            {
+                e.Handled = true;
+            }
+        }
+        private static bool IsTextAllowed(string text)
+        {
+            // Проверка, что вводимые символы - это цифры
+            return Regex.IsMatch(text, @"^[0-9]+$");
+        }
+
+        private void PhoneTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            PhoneTextBox.TextChanged -= PhoneTextBox_TextChanged;
+
+            // Получаем текст без пробелов
+            string input = PhoneTextBox.Text.Replace(" ", "");
+
+            if (input.Length > 12)
+            {
+                input = input.Substring(0, 12);
+            }
+
+            // Форматируем номер телефона
+            if (input.Length > 0)
+            {
+                // Добавляем пробелы в нужные места
+                string formatted = "";
+
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (i == 2 || i == 5 || i == 8 || i == 10) // Добавляем пробелы после 3 и 6 символов
+                    {
+                        formatted += " ";
+                    }
+                    formatted += input[i];
+                }
+
+                // Обновляем текст в TextBox
+                PhoneTextBox.Text = formatted;
+                // Устанавливаем курсор в конец текста
+                PhoneTextBox.SelectionStart = PhoneTextBox.Text.Length;
+            }
+
+            // Восстанавливаем обработчик события
+            PhoneTextBox.TextChanged += PhoneTextBox_TextChanged;
+        }
+    }
+}
---
+++ UchebkaDEMO/UchebkaDEMO/ViewPartnersWindow.xaml.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UchebkaDEMO
+{
+    /// <summary>
+    /// Логика взаимодействия для ViewPartnersWindow.xaml
+    /// </summary>
+    public partial class ViewPartnersWindow : Window
+    {
+        public ViewPartnersWindow()
+        {
+            InitializeComponent();
+            // Получение текущей даты и времени
+            DateTime currentDateTime = DateTime.Now;
+            // Форматирование даты
+            string formattedDate = currentDateTime.ToString("dd-MM-yyyy");
+            TodayDateLabel.Content = $"Сегодня: {formattedDate}";
+
+            LoadInfo();
+        }
+
+        private void LoadInfo(string filter = null, string typeComp = null, string comboBoxFilter = null)
+        {
+            this.DataContext = this;
+            string connectionString = @"Data Source=DESKTOP-DC3OGQJ; Initial Catalog=DemoUCheb;Integrated Security=True"; // Замените на вашу строку подключения к БД
+            string query = @"
+                SELECT p.id,
+                    p.name, 
+                    p.firstname, 
+                    p.secondname, 
+                    p.patronymic, 
+                    p.phone, 
+                    p.rating, 
+                    COALESCE(SUM(po.qty), 0) AS Volume, 
+                    ct.name AS Company_type 
+                FROM 
+                    Partners p 
+                LEFT JOIN 
+                    Orders o ON p.id = o.partner_id 
+                lEFT JOIN 
+                    Product_orders po ON o.id = po.order_id 
+                LEFT JOIN 
+                    Company_types ct ON ct.id = p.type_id";
+
+            if (!string.IsNullOrEmpty(filter))
+            {
+                query += " WHERE p.name LIKE @Filter OR p.firstname LIKE @Filter" +
+                    " OR p.secondname LIKE @Filter OR p.patronymic LIKE @Filter" +
+                    " OR p.phone LIKE @Filter " +
+                    "GROUP BY p.id, p.name, p.firstname, p.secondname, p.patronymic, p.phone, p.rating, ct.name";
+            }
+            else if (!string.IsNullOrEmpty(typeComp))
+            {
+                query += " WHERE ct.name = @compType " +
+                "GROUP BY p.id, p.name, p.firstname, p.secondname, p.patronymic, p.phone, p.rating, ct.name";
+             
+            }
+            else if (!string.IsNullOrEmpty(comboBoxFilter))
+            {
+                if (comboBoxFilter == "По возрастанию рейтинга") 
+                {
+                    query += " GROUP BY p.id, p.name, p.firstname, p.secondname, p.patronymic, p.phone, p.rating, ct.name " +
+                        "ORDER BY rating ASC";
+                }
+                else if (comboBoxFilter == "По убыванию рейтинга") 
+                {
+                    query += " GROUP BY p.id, p.name, p.firstname, p.secondname, p.patronymic, p.phone, p.rating, ct.name " +
+                        "ORDER BY rating DESC";
+                }
+            }
+            else
+            {
+                query += " GROUP BY p.id, p.name, p.firstname, p.secondname, p.patronymic, p.phone, p.rating, ct.name";
+            }
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand(query, connection);
+
+                    if (!string.IsNullOrEmpty(filter))
+                    {
+                        command.Parameters.AddWithValue("@Filter", "%" + filter + "%");
+                    }
+                    else if (!string.IsNullOrEmpty(typeComp))
+                    {
+                        command.Parameters.AddWithValue("@compType", typeComp);
+                    }
+
+                    SqlDataReader reader = command.ExecuteReader();
+                    
+                    while (reader.Read())
+                    {
+                        long totalSold = Convert.ToInt64(reader["Volume"]);
+                        string discount = CalcDiscount(totalSold);
+                        StackPanel partnerPanel = new StackPanel
+                        {
+                            Orientation = Orientation.Vertical,
+                            Margin = new Thickness(10),
+                            Background = new SolidColorBrush(Colors.White)
+                        };
+
+                        // Получите идентификатор партнера (предполагается, что он есть в выборке)
+                        int partnerId = Convert.ToInt32(reader["id"]); // Замените "id" на фактическое имя столбца
+
+                        // Назначьте обработчик события MouseDown
+                        partnerPanel.MouseDown += (s, e) => PartnerStackPanel_MouseDown(s, e, partnerId);
+
+                        Grid grid = new Grid
+                        {
+                                Margin = new Thickness(5,5,5,0),
+                        };
+
+                        grid.Children.Add(new TextBlock
+                        {
+                            Text = $"{reader["Company_type"]}| {reader["name"]}",
+                            FontSize = 16,
+                            FontWeight = FontWeights.Bold,
+                            HorizontalAlignment = HorizontalAlignment.Left,
+                        });
+
+                        grid.Children.Add(new TextBlock
+                        {
+                            Text = discount,
+                            FontSize = 16,
+                            FontWeight = FontWeights.Bold,
+                            HorizontalAlignment = HorizontalAlignment.Right,
+                        });
+                        
+                        partnerPanel.Children.Add(grid);
+
+                        partnerPanel.Children.Add(new TextBlock
+                        {
+                            Text = $"Директор: {reader["firstname"]} {reader["secondname"]} {reader["patronymic"]}",
+                            FontSize = 12,
+                            Margin = new Thickness(5,0,5,0),
+                        });
+
+                        partnerPanel.Children.Add(new TextBlock
+                        {
+                            Text = $"+7 {reader["phone"]}",
+                            FontSize = 12,
+                            Margin = new Thickness(5, 0, 5, 0),
+                        });
+
+                        partnerPanel.Children.Add(new TextBlock
+                        {
+                            Text = $"Рейтинг: {reader["rating"]}",
+                            FontSize = 12,
+                            Margin = new Thickness(5, 0, 5, 5),
+                        });
+
+                        PartnerStackPanel.Children.Add(partnerPanel);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при загрузке данных: {ex}");
+            }
+        }
+
+        public static string CalcDiscount(Int64 volume)
+        {
+            string discount;
+            if (volume < 10000)
+            {
+                discount = "0%"; // 0%
+            }
+            else if (volume >= 10000 && volume < 50000)
+            {
+                discount = "5%"; // 5%
+            }
+            else if (volume >= 50000 && volume < 300000)
+            {
+                discount = "10%"; // 10%
+            }
+            else
+            {
+                discount = "15%"; // 15%
+            }
+            return discount;
+        } 
+
+        private void Back_Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+
+        private void AddInfo_Button_Click(object sender, RoutedEventArgs e)
+        {
+            AddPartnerWindow addPartnerWindow = new AddPartnerWindow();
+            addPartnerWindow.Show();
+            this.Close();
+        }
+
+        private void SearchTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            FilterComboBox.SelectedItem = null;
+            CompanyTypeComboBox.SelectedItem = null;
+            CompanyTypeComboBox.Visibility = Visibility.Collapsed;
+            PartnerStackPanel.Children.Clear();
+            LoadInfo(SearchTextBox.Text);
+        }
+
+        private void CompanyTypeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (CompanyTypeComboBox.SelectedItem is ComboBoxItem selectedItem)
+            {
+                string selectedText = selectedItem.Content.ToString();
+                PartnerStackPanel.Children.Clear();
+                LoadInfo(null, selectedText);
+            }
+        }
+
+        private void FilterComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (FilterComboBox.SelectedItem is ComboBoxItem selectedItem)
+            {
+                if (selectedItem.Content.ToString() == "По типу")
+                {
+                    CompanyTypeComboBox.Visibility = Visibility.Visible;
+                }
+                else if (selectedItem.Content.ToString() != "По типу"
+                    && !string.IsNullOrEmpty(selectedItem.Content.ToString()))
+                {
+                    string selectedText = selectedItem.Content.ToString();
+                    PartnerStackPanel.Children.Clear();
+                    LoadInfo(null, null, selectedText);
+                }
+                else { CompanyTypeComboBox.Visibility = Visibility.Collapsed; }
+            }
+        }
+
+        private void PartnerStackPanel_MouseDown(object sender, MouseButtonEventArgs e, int partId)
+        {
+            EditPartnerWindow editWindow = new EditPartnerWindow(partId);
+            editWindow.Show();
+            this.Close();
+        }
+    }
+}
